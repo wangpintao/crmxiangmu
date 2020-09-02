1b9c67b21b5a1fba9d3c46dd411880f9b7b7fc79
Wang <3506882025@qq.com>
2020-09-02 17:57:32 +0800

    (王乔阳)

--- src/main/java/com/huayu/controller/DocumentaryController.java
+++ src/main/java/com/huayu/controller/DocumentaryController.java
@@ -15,9 +15,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/documentary")
@@ -56,17 +62,29 @@ public class DocumentaryController {
         List<Commercial> list=iCommercialServiceImp.list();
         //跟单分类
         List<DocClassify> list1=iDocClassifyServiceImp.list();
-        System.out.println(list+"=="+list1);
         model.addAttribute("shang",list);
         model.addAttribute("gen",list1);
 
-        return "forward:/documentary/documentaryadd.html";
+        return "/documentary/documentaryadd";
     }
 
     @PostMapping("/add1.do")
-    public String add1(Documentary documentary){
-        iDocumentaryServiceImp.save(documentary);
-
+    public String add1(@RequestParam("file") MultipartFile docfile,Documentary documentary,HttpServletRequest request){
+        try {
+            String oriName = docfile.getOriginalFilename();
+            System.out.println(oriName+"="+documentary.getDocuser());
+            String path = request.getServletContext().getRealPath("/upload");
+            File file = new File(path);
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+            docfile.transferTo(new File(path, oriName));
+            DocClassify docClassify=iDocClassifyServiceImp.getById(documentary.getDocClaid());
+            documentary.setTheme(documentary.getTheme()+docClassify.getClaname());
+            iDocumentaryServiceImp.save(documentary);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return "/documentary/documentary.html";
     }
 }
