d0e2acb091ee917239a88f924f622613da1a2edf
Wang <3506882025@qq.com>
2020-09-08 08:51:14 +0800

    (王乔阳) 9/8 8:50

--- src/main/java/com/huayu/service/imp/IForumServiceImp.java
+++ src/main/java/com/huayu/service/imp/IForumServiceImp.java
@@ -4,4 +4,9 @@ import com.huayu.pojo.Forum;
 import com.huayu.pojo.Permission;
 
 public interface IForumServiceImp extends IService<Forum>{
+    public Integer updateone(Forum forum);
+
+    public Integer updateone1(Forum forum);
+
+    public Integer updateone2(Forum forum);
 }
