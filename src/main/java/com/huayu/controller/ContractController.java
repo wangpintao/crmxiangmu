af44f58859f48b5a172b4e6fd4c33fbf2b800cd9
Wang <3506882025@qq.com>
2020-09-09 09:16:15 +0800

    (王乔阳) 9/9 9:50

--- src/main/java/com/huayu/controller/ContractController.java
+++ src/main/java/com/huayu/controller/ContractController.java
@@ -1,11 +1,20 @@
 package com.huayu.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.huayu.JsonUtils.StulayuiJson;
+import com.huayu.layuiUtils.Stulayui;
 import com.huayu.pojo.Contract;
 import com.huayu.pojo.Forum;
+import com.huayu.pojo.UserClien;
 import com.huayu.service.imp.IContractServiceImp;
 import com.huayu.service.imp.IForumServiceImp;
+import com.huayu.service.imp.IUserClienServiceImp;
+import io.swagger.models.auth.In;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,9 +25,41 @@ import java.util.List;
 public class ContractController {
     @Autowired
     private IContractServiceImp iContractServiceImp;
+    @Autowired
+    private IUserClienServiceImp iUserClienServiceImp;
+
     @RequestMapping("/queryall.do")
     @ResponseBody
-    public List<Contract> queryall(){
-        return iContractServiceImp.list();
+    public Stulayui queryall(Integer page, Integer limit, Contract contract){
+        System.out.println(contract);
+        Stulayui stulayui=new Stulayui();
+        Page<Contract> page1=new Page<>(page,limit);
+        List<Contract> list=iContractServiceImp.queryall(page1,contract);
+        if(list.size()>0){
+            stulayui.setCode(0);
+            stulayui.setMsg("查询成功");
+            stulayui.setCount(list.size());
+            stulayui.setData(list);
+        }else{
+            stulayui.setCode(1);
+            stulayui.setMsg("无数据");
+            stulayui.setCount(list.size());
+            stulayui.setData(list);
+        }
+        return stulayui;
+    }
+
+    @RequestMapping("/add.do")
+    public String queryall(Model model){
+        model.addAttribute("list",iUserClienServiceImp.list());
+        return "/contract/add.html";
+    }
+
+    @PostMapping("/user.do")
+    @ResponseBody
+    public StulayuiJson user(Integer ucid){
+        StulayuiJson stulayuiJson=new StulayuiJson();
+        stulayuiJson.setData(iUserClienServiceImp.selectId(ucid));
+        return stulayuiJson;
     }
 }
--- src/main/java/com/huayu/mapper/ContractMapper.java
+++ src/main/java/com/huayu/mapper/ContractMapper.java
@@ -1,11 +1,12 @@
 package com.huayu.mapper;
-
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.huayu.comment.ContractSelectProvider;
+import org.apache.ibatis.annotations.*;
 import com.huayu.pojo.Contract;
-import com.huayu.pojo.DocStatus;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.SelectProvider;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.List;
 
 /**
  * <p>
@@ -20,36 +21,8 @@ public interface ContractMapper extends BaseMapper<Contract> {
     @Select("select * from contract where serial =#{serial}")
     Contract queryByConId(String serial);
 
-    //本周合同机数
-    @Select("SELECT COUNT(con_startdate) FROM contract WHERE YEARWEEK(DATE_FORMAT(con_startdate,'%Y-%m-%d')) = YEARWEEK(NOW())")
-    Integer ConWeek();
 
-    //上周合同数
-    @Select("SELECT COUNT(con_startdate) FROM contract WHERE YEARWEEK(DATE_FORMAT(con_startdate,'%Y-%m-%d')) = YEARWEEK(NOW())-1")
-    Integer ConLaerWeek();
-
-    //本月合同数
-    @Select("SELECT COUNT(con_startdate) FROM contract WHERE DATE_FORMAT( con_startdate, '%Y%m' ) = DATE_FORMAT( CURDATE( ) , '%Y%m' )")
-    Integer ConMonth();
-
-    //上月合同数
-    @Select("SELECT COUNT(con_startdate) FROM contract WHERE PERIOD_DIFF( DATE_FORMAT( NOW( ) , '%Y%m' ) , DATE_FORMAT( con_startdate, '%Y%m' ) ) =1")
-    Integer ConLaerMonth();
-
-    //本季合同数
-    @Select("SELECT COUNT(con_startdate) FROM contract WHERE QUARTER(con_startdate)=QUARTER(NOW())")
-    Integer ConSeason();
-
-    //上季合同数
-    @Select("SELECT COUNT(con_startdate) FROM contract WHERE QUARTER(con_startdate)=QUARTER(DATE_SUB(NOW(),INTERVAL 1 QUARTER))")
-    Integer ConLaerSeason();
-
-    //本年合同数
-    @Select("SELECT COUNT(con_startdate) FROM contract WHERE YEAR(con_startdate)=YEAR(NOW())")
-    Integer ConYear();
-
-    //去年合同数
-    @Select("SELECT COUNT(con_startdate) FROM contract WHERE YEAR(con_startdate)=YEAR(DATE_SUB(NOW(),INTERVAL 1 YEAR))")
-    Integer ConLaerYear();
+    @SelectProvider(type = ContractSelectProvider.class,method ="queryall")
+    public List<Contract> queryall(@Param("page") IPage<Contract> page, @Param("con") Contract contract);
 
 }
